26519e890db07d9b21b54625def2ffcc4496a69f
Marcos Roberto Meneghette <marcos@siq.com.br>
2023-05-02 10:26:59 -0300

    w

--- src/br/com/siq/Test.java
+++ src/br/com/siq/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("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");
+		
+		System.out.println("2365");
 	}
 
 }
